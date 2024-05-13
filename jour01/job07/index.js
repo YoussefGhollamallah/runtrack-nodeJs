8a66900de61a5223e376f1996bb35d0c4e4fd655
YoussefGhollamallah <youssef.gh@live.fr>
2024-05-13 16:04:51 +0200

    mise a jour de l'import du fichier readFile

--- jour01/job07/index.js
+++ jour01/job07/index.js
@@ -1,15 +1,13 @@
-// import fs from "node:fs"
+import {readFile} from "fs"
 
-// const content = fs.readFile("data.txt", {encoding:"utf-8"}, function (err,data) {
-//     console.log(data)
-// })
-
-// console.log("salut")
-
+readFile("jour01/job07/data.txt", "utf-8", function (err, data) {
+    const content = data;
+    console.log(content);
+});
 
 //  autre manière d'afficher le texte en asynchrone avec les promises
 
-import { readFile } from "node:fs/promises"
+// import { readFile } from "node:fs/promises"
 
-const newContent = await readFile("newData.txt", {encoding: "utf8"})
-console.log(newContent)
+// const newContent = await readFile("newData.txt", {encoding: "utf8"})
+// console.log(newContent)
