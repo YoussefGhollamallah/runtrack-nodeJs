6deb65026be4f8b5f1014f95c46d375c3cb39f1f
YoussefGhollamallah <youssef.gh@live.fr>
2024-05-13 15:22:56 +0200

    modification et affichage en méthode asynchrone

--- jour01/job07/index.js
+++ jour01/job07/index.js
@@ -1,14 +1,7 @@
-const fs = require("fs")
+import fs from "node:fs"
 
-const filename = "D:/runtrack-nodeJs/jour01/job07/data.txt"
-
-fs.appendFile(filename, "texte ajouter \r", (err)=> {
-    if (err) throw err;
-    console.log('Le fichier a été mis à jour');
-	fs.readFile(filename, 'utf8', (err, data) => {
-		if (err) throw err;
-		console.log(data);
-	});
+const content = fs.readFile("data.txt", {encoding:"utf-8"}, function (err,data) {
+    console.log(data)
 })
 
-console.log("affichage asynchrone activé")
+console.log("salut")
