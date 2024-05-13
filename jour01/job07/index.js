bb48232e601e806e23b64077c5e323053f719d3f
YoussefGhollamallah <youssef.gh@live.fr>
2024-05-13 15:31:29 +0200

    ajout d'une autre manière d'afficher en asynchrone avec les promises

--- jour01/job07/index.js
+++ jour01/job07/index.js
@@ -1,7 +1,15 @@
-import fs from "node:fs"
+// import fs from "node:fs"
 
-const content = fs.readFile("data.txt", {encoding:"utf-8"}, function (err,data) {
-    console.log(data)
-})
+// const content = fs.readFile("data.txt", {encoding:"utf-8"}, function (err,data) {
+//     console.log(data)
+// })
 
-console.log("salut")
+// console.log("salut")
+
+
+//  autre manière d'afficher le texte en asynchrone avec les promises
+
+import { readFile } from "node:fs/promises"
+
+const newContent = await readFile("newData.txt", {encoding: "utf8"})
+console.log(newContent)
