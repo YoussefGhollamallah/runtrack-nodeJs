6c080c40914960e794a3232b10b352f59a5d5bb6
YoussefGhollamallah <youssef.gh@live.fr>
2024-05-13 14:17:17 +0200

    modification de la fonction de lecture du module fs en méthode asynchrone

--- jour01/job06/index.js
+++ jour01/job06/index.js
@@ -1,9 +1,9 @@
-const fs = require("fs")
+const fs = require("node:fs")
 
 
 const filename = "D:/runtrack-nodeJs/jou01/data.txt"
 
-fs.readFile(filename,"utf8", (err, data) => { 
+fs.readFileSync(filename,"utf8", (err, data) => { 
     if (err) throw err;
     console.log(data);
 })
