19a14705cf6142ba37b2e64c43734a2876a2c967
YoussefGhollamallah <youssef.gh@live.fr>
2024-05-15 00:11:33 +0200

    ajout de la constante qui permet de récupérer le nom d'hôte de l'url fourni

--- jour01/job10/index.js
+++ jour01/job10/index.js
@@ -2,6 +2,11 @@ const url = require("url")
 
 const URL = url.parse("https://www.google.com&search=nodejs")
 
-
+// récupération du protocole 
 const protocole = URL.protocol
-console.log(protocole)
+console.log("Le protocole utilisé est : " + protocole)
+
+
+// récupération du nom d'hôte
+const hostname = URL.hostname
+console.log("Le nom d'hôte est : " + hostname)
