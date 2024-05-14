aa497fdbe448fad6728b943715112e18d4a461a7
YoussefGhollamallah <youssef.gh@live.fr>
2024-05-15 01:19:16 +0200

    récupération des paramètre de l'url

--- jour01/job10/index.js
+++ jour01/job10/index.js
@@ -1,6 +1,6 @@
 const url = require("url")
 
-const URL = url.parse("https://www.google.com&search=nodejs")
+const URL = url.parse("https://www.google.com/search?q=nodejs&safe=active&ssui=on")
 
 // récupération du protocole 
 const protocole = URL.protocol
@@ -10,3 +10,7 @@ console.log("Le protocole utilisé est : " + protocole)
 // récupération du nom d'hôte
 const hostname = URL.hostname
 console.log("Le nom d'hôte est : " + hostname)
+
+// récupération des paramètres
+const params = URL.search
+console.log("Voici les paramaètres : " + params)
