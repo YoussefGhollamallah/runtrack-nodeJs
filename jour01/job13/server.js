d84cabafa07962254100632ed8421878b887c4cb
YoussefGhollamallah <youssef.gh@live.fr>
2024-05-16 00:57:04 +0200

    mise en place de redirection à partir de l'url

--- jour01/job13/server.js
+++ jour01/job13/server.js
@@ -1,16 +1,31 @@
-import {createServer} from "node:http"
-import { createReadStream } from "node:fs"
+import { createServer } from 'node:http';
+import { createReadStream } from 'node:fs';
+import { parse } from 'node:url';
 
-const server = createServer((req,res) => {
-    const file = createReadStream("index.html");
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
-    file.pipe(res, {end: false})
-    file.on("end", () => {
-        res.end()
-    })
-    
-})
+function router(req, res) {
+    const path = parse(req.url).pathname;
 
-server.listen("8888")
+    if (path === '/') {
+        const file = createReadStream('index.html');
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        file.pipe(res, { end: false });
+        file.on('end', () => {
+            res.end();
+        });
+    } else if (path === '/about') {
+        const file = createReadStream('about.html');
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        file.pipe(res, { end: false });
+        file.on('end', () => {
+            res.end();
+        });
+    }
+}
+
+const server = createServer(router);
+
+server.listen(8888);
