9f79af26a511600deacc99a8135e64b62fb9daf2
YoussefGhollamallah <youssef.gh@live.fr>
2024-05-15 15:07:32 +0200

    test de correction de push

--- jour01/job12/server.js
+++ jour01/job12/server.js
@@ -3,6 +3,9 @@ import { createReadStream } from "node:fs"
 
 const server = createServer((req,res) => {
     const file = createReadStream("index.html");
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
     file.pipe(res, {end: false})
     file.on("end", () => {
         res.end()
