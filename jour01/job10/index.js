5d33a779d675d742cf22fdaaee157db28605a883
YoussefGhollamallah <youssef.gh@live.fr>
2024-05-15 10:33:12 +0200

    modification et création d'une nouvelle URL

--- jour01/job10/index.js
+++ jour01/job10/index.js
@@ -14,3 +14,16 @@ console.log("Le nom d'hôte est : " + hostname)
 // récupération des paramètres
 const params = URL.search
 console.log("Voici les paramaètres : " + params)
+
+
+URL.host = "www.laplateforme.io"
+console.log(URL.host)
+
+URL.search = "?lang=fr"
+console.log(URL.search)
+
+
+const newURLString = url.format(URL)
+const newURL = url.parse(newURLString)
+console.log("Le protocole est " + newURL.protocol)
+console.log("Nouvelle URL " + newURL.href)
