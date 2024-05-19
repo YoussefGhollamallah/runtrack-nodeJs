d76ade76f9a664defd4157dc99344bf3f2c58017
YoussefGhollamallah <youssef.gh@live.fr>
2024-05-19 21:15:41 +0200

    Job 4 : création de deux routes avec express

--- runtrack_express/projet_web/index.js
+++ runtrack_express/projet_web/index.js
@@ -6,3 +6,12 @@ const app = express()
 app.listen(port, () => {
     console.log("Serveur actif sur le port: ", port)
 })
+
+app.get("/", (req, res) => {
+    res.send("Bienvenue sur la page d'acceuil")
+} )
+
+
+app.get("/about", (req, res) => {
+    res.send("Bienvenue sur la page de projet")
+} )
